d0a742b4f6e00b307f1d5d9562ecad441585fa88
Solange Ohana <solangeohana@gmail.com>
2021-01-31 20:25:43 +0100

    [deploy] update deployment

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ if command -v pm2 &> /dev/null; then
     if [ ! -L "/var/www/public" ]; then
         ln -s /var/www/darna-website/client/build /var/www/public
     fi
-    sudo -u nodejs pm2 restart darna
+    sudo -u nodejs pm2 restart server
     sudo systemctl restart nginx
 fi
