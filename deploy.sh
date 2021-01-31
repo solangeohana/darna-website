ca241873e6b69125906f845166f6c79e86db1095
Solange Ohana <solangeohana@gmail.com>
2021-01-31 18:54:08 +0100

    [deploy] update deployment

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ cd client && npm install && npm run build && cd ..
 
 if command -v pm2 &> /dev/null
 then
-    sudo -u nodejs pm2 restart
+    sudo -u nodejs rm -rf /var/www/public && sudo -u nodejs cp -R client/build /var/www/public/
+    sudo -u nodejs pm2 restart darna
     sudo systemctl restart nginx
 fi
